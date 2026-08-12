d17f9635b571a62355ef5a40c9f7cf6b8dfade43
mel-bir <261373230+mel-bir@users.noreply.github.com>
2026-08-12 11:34:47 +0300

    added the function operate that check the given operator and call the correct operation function

--- java.js
+++ java.js
@@ -13,3 +13,22 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
+
+function operate(a,operator,b){
+    switch(operator){
+        case "+":
+           return add(a,b);
+            break;
+        case "-":
+           return substract(a,b);
+            break;
+        case "*":
+           return multiply(a,b);
+            break;
+        case "/":
+           return divide(a,b);
+            break; 
+        default:
+            return "Invalid Operator";       
+    }
+}
